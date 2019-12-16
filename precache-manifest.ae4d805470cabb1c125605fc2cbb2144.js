0749048bef9a526c1d77f01c6b16bb3bbb09b4ff
fengjinpeng <fengjinpeng@56foryou.com>
2019-12-16 14:18:16 +0800

    Updates

---
+++ precache-manifest.ae4d805470cabb1c125605fc2cbb2144.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0f46267e985aecd49e7880914583a6",
+    "url": "/abd-demo/index.html"
+  },
+  {
+    "revision": "353516aae73faa5d4f93",
+    "url": "/abd-demo/static/css/2.210db8d4.chunk.css"
+  },
+  {
+    "revision": "a0465209b4747aa1692d",
+    "url": "/abd-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "353516aae73faa5d4f93",
+    "url": "/abd-demo/static/js/2.63851869.chunk.js"
+  },
+  {
+    "revision": "e9c4d9e04e4f9bae83c8ba152a453530",
+    "url": "/abd-demo/static/js/2.63851869.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0465209b4747aa1692d",
+    "url": "/abd-demo/static/js/main.ee900056.chunk.js"
+  },
+  {
+    "revision": "2f052458eac4370c650d",
+    "url": "/abd-demo/static/js/runtime-main.101ea173.js"
+  },
+  {
+    "revision": "2dc5f250598fc910d2b30ac047bc392e",
+    "url": "/abd-demo/static/media/index.2dc5f250.less"
+  },
+  {
+    "revision": "c17ec543e0d6a592928535f43867f969",
+    "url": "/abd-demo/static/media/index.c17ec543.less"
+  },
+  {
+    "revision": "d012eecee6ab14418860157e33c28c9c",
+    "url": "/abd-demo/static/media/index.d012eece.less"
+  }
+]);
